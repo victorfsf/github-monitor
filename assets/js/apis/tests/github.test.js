4c7bc52393e96b8a6b695df1388bc0fe2566df50
Victor Ferraz <victorfsf.dev@gmail.com>
2018-04-09 14:55:26 -0300

    [master] Finish github.test.js

--- assets/js/apis/tests/github.test.js
+++ assets/js/apis/tests/github.test.js
@@ -51,4 +51,40 @@ describe('GithubAPI', () => {
       'getBranches',
     );
   });
+
+  test('getCommits', () => {
+    GithubAPI.getBranches = jest.fn().mockReturnValue(
+      Promise.resolve([
+        { name: 'staging' },
+        { name: 'master' },
+      ]),
+    );
+    window.fetch = jest.fn().mockReturnValueOnce(Promise.resolve({
+      json: () => [
+        { sha: 1 }, { sha: 1 },
+        { sha: 2 }, { sha: 3 },
+      ],
+    })).mockReturnValueOnce(Promise.resolve({
+      json: () => [
+        { sha: 4 }, { sha: 4 }, { sha: 4 },
+        { sha: 5 }, { sha: 5 },
+        { sha: 6 }, { sha: 6 },
+      ],
+    }));
+    GithubAPI.getCommits('test/test').then(
+      (response) => {
+        expect(response).toMatchObject({
+          ok: true,
+          commits: [
+            { branch: 'staging', sha: 1 },
+            { branch: 'staging', sha: 2 },
+            { branch: 'staging', sha: 3 },
+            { branch: 'master', sha: 4 },
+            { branch: 'master', sha: 5 },
+            { branch: 'master', sha: 6 },
+          ],
+        });
+      },
+    );
+  });
 });
